77e01665ad5e3b7dac050a5daacd9b65eacbc84d
sreg23 <146397088+sreg23@users.noreply.github.com>
2026-02-07 11:28:42 -0800

    Update main.js

--- assets/   js/main.js
+++ assets/   js/main.js
@@ -1,1 +1,12 @@
+// assets/js/main.js
+
+document.addEventListener("DOMContentLoaded", () => {
+  const currentPath = window.location.pathname;
+
+  document.querySelectorAll(".nav__link").forEach(link => {
+    if (link.getAttribute("href") === currentPath) {
+      link.classList.add("nav__link--active");
+    }
+  });
+});
 
